0a8ef7026986996fec201637e9be4c940ea4df9a
francomarinozzi <francomarinozzi4@gmail.com>
2025-03-13 18:06:39 -0300

    new endpoint

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -31,7 +31,7 @@ const login = async(req,res) =>{
         if(!user){
             return res.status(400).json({message:`Usuario con el correo ${email} no encontrado.`})
         }
-
+        
         const match = await bcrypt.compare(password, user.password)
 
         if(!match){
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,15 @@
 const { Router } = require('express') 
 const userControllers = require('../controllers/userControllers')
 const router = Router()
-
+const auth = require('../middleware/session')
 
 router.post('/register', userControllers.register)
 router.post('/login',userControllers.login)
 router.post('/logout', userControllers.logout)
+
+router.get('/session_check', auth, (req,res) =>{  
+    res.status(200).json({message:`Sesión iniciada`})
+})
+
 
 module.exports = router
